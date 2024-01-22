17716ef54415b9069e822e3f648316d469d4134f
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-22 16:49:38 +0100

    modules updated

--- src/app/services/auth/auth.module.ts
+++ src/app/services/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { IonicStorageModule } from '@ionic/storage';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 @NgModule({
   declarations: [],
